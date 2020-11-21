2326aaea16f3f91c8df12c6f3575fde6073fe9da
Marcel <keamas@gmail.com>
2020-11-21 07:25:39 +0100

    Create dsc-win-tools.ps1

---
+++ dsc-win-tools.ps1
@@ -0,0 +1,14 @@
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable --name=allowGlobalConfirmation  --yes=true
+choco install microsoft-edge
+choco install firefox
+choco install putty.install
+choco install winscp.install
+choco install 7zip.install
+choco install notepadplusplus.install
+choco install wireshark
+choco install azure-cli
+choco install aksctl
+(new-object Net.WebClient).DownloadString("https://aka.ms/AzureCT") | Invoke-Expression
+Install-LinkPerformance -Force
