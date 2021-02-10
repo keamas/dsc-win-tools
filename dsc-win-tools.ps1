46535258e995e111c790e833a42d9bb4d9440f3b
Marcel <keamas@gmail.com>
2021-02-10 11:19:42 +0100

    Update dsc-win-tools.ps1

--- dsc-win-tools.ps1
+++ dsc-win-tools.ps1
@@ -9,8 +9,8 @@ choco install 7zip.install
 choco install notepadplusplus.install
 choco install wireshark
 choco install azure-cli
-choco install dbeaver
-choco install sql-server-management-studio
+#choco install dbeaver
+#choco install sql-server-management-studio
 (new-object Net.WebClient).DownloadString("https://aka.ms/AzureCT") | Invoke-Expression
 Install-LinkPerformance -Force
 Install-WindowsFeature -Name Web-Server
